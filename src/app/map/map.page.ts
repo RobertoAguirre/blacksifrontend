efb163c842d1c71df175c24594a91a4b415e680d
RobertoAguirre <roberto.aguirre.rodriguez@gmail.com>
2023-08-21 13:17:32 -0600

    eta working

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -18,6 +18,11 @@ export class MapPage implements OnInit {
   estimatedTravelTimes: string[] = [];
   selectedWaypoint: string;
 
+  //calculate distance between pints
+  public totalDistance: number = 0; // To store the total distance of the route
+  public totalTravelTime: number = 0; // To store the total travel time in minutes
+
+
   constructor(
     private activatedRoute: ActivatedRoute, // Inject ActivatedRoute
     private router: Router // Inject Router
@@ -52,6 +57,56 @@ export class MapPage implements OnInit {
 
   }
 
+
+  public calculateAndDisplayRoute() {
+    // Exit if there are no locations to draw a route
+    if (this.currLocations.length === 0) {
+      console.error('No locations provided to calculate the route.');
+      return;
+    }
+  
+    const waypoints = this.currLocations.slice(1, this.currLocations.length - 1).map(location => ({ location: location }));
+  
+    const request = {
+      origin: this.currLocations[0], // Start from the first location
+      destination: this.currLocations[this.currLocations.length - 1], // End at the last location
+      waypoints: waypoints,
+      optimizeWaypoints: true, // Optimize the order of waypoints for the shortest route
+      travelMode: 'DRIVING'
+    };
+  
+    this.directionsService.route(request, (response, status) => {
+      if (status === 'OK') {
+        this.directionsRenderer.setDirections(response);
+        
+        // Reset the travel times array
+        this.estimatedTravelTimes = [];
+  
+        // Calculating the total distance, total travel time and individual travel times
+        let totalDistanceMeters = 0;
+        let totalDurationSeconds = 0;
+        const legs = response.routes[0].legs;
+        for (let i = 0; i < legs.length; i++) {
+          totalDistanceMeters += legs[i].distance.value;
+          totalDurationSeconds += legs[i].duration.value;
+  
+          // Convert individual leg duration to minutes and push to estimatedTravelTimes
+          const legDurationMinutes = Math.floor(legs[i].duration.value / 60);
+          this.estimatedTravelTimes.push(`${legDurationMinutes} minutes`);
+        }
+        this.totalDistance = totalDistanceMeters / 1000; // Convert to kilometers
+        this.totalTravelTime = totalDurationSeconds / 60; // Convert to minutes
+        
+        console.log(`Total distance: ${this.totalDistance.toFixed(2)} km`);
+        console.log(`Total estimated travel time: ${Math.floor(this.totalTravelTime)} minutes`);
+      } else {
+        console.error('Directions request failed due to', status);
+      }
+    });
+  }
+  
+
+
   public initializeMap() {
     const mapOptions = {
       center: { lat: 28.637240, lng: -106.075057 }, // Set the initial position of the map
@@ -75,8 +130,14 @@ export class MapPage implements OnInit {
         }).catch(error => {
             console.error('Error placing marker for address:', address, 'Error:', error);
         });
+      }
+
+      // Call the function to calculate and display the route
+    this.calculateAndDisplayRoute();
+
     }
-    }
+
+    
 
 
   }
